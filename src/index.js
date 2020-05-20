cddf200dadf85eec6ff23a349ba5793187bc16f3
Quentin Dauprat <heziode@protonmail.com>
2020-05-20 22:56:31 +0200

    :bug: Fix issue related to default quality property with png type (#28)
    Co-authored-by: FRIN Yvonnick <frin.yvonnick@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ module.exports = async function({
   output,
   type,
   content,
-  quality = 80, // only applicable for jpg
+  quality,
   waitUntil = 'load',
   transparent = false,
   puppeteerArgs = {},
@@ -14,6 +14,10 @@ module.exports = async function({
 }) {
   if (!html) {
     throw Error('You must provide an html property.')
+  }
+  let screeshotArgs = {}
+  if (type === 'jpeg') {
+    screeshotArgs.quality = quality ? quality : 80
   }
   const browser = await puppeteer.launch({ ...puppeteerArgs, headless: true })
   const page = await browser.newPage()
@@ -23,7 +27,7 @@ module.exports = async function({
   }
   await page.setContent(html)
   const element = await page.$('body')
-  const buffer = await element.screenshot({ path: output, type, quality, omitBackground: transparent, encoding })
+  const buffer = await element.screenshot({ path: output, type, omitBackground: transparent, encoding, ...screeshotArgs })
   await browser.close()
   return buffer
 }
