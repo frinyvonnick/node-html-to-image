16924027bcee12c0be53eaa0bc6a144bd0901ddf
FRIN Yvonnick <frin.yvonnick@gmail.com>
2020-05-20 14:55:02 +0200

    :white_check_mark: Add tests for quality option

---
+++ src/quality.spec.js
@@ -0,0 +1,56 @@
+const nodeHtmlToImage = require('./index.js')
+const puppeteer = require('puppeteer')
+
+describe('quality', () => {
+  let screenshot
+  let puppeteer
+
+  beforeEach(() => {
+    puppeteer = require('puppeteer')
+    screenshot = puppeteer.launch().newPage().$().screenshot
+  })
+
+  it('should not set quality option for png images', async () => {
+    await nodeHtmlToImage({
+      type: 'png',
+      quality: 300,
+      html: '<html><body>Hello world!</body></html>',
+    })
+    
+    expect(screenshot).toHaveBeenCalledWith(expect.objectContaining({ quality: undefined }))
+  })
+
+  it('should set quality option for jpg images', async () => {
+    await nodeHtmlToImage({
+      type: 'jpeg',
+      quality: 30,
+      html: '<html><body>Hello world!</body></html>',
+    })
+    
+    expect(screenshot).toHaveBeenCalledWith(expect.objectContaining({ quality: 30 }))
+  })
+
+  it('should set quality option to 80 by default for jpg images', async () => {
+    await nodeHtmlToImage({
+      type: 'jpeg',
+      html: '<html><body>Hello world!</body></html>',
+    })
+    
+    expect(screenshot).toHaveBeenCalledWith(expect.objectContaining({ quality: 80 }))
+  })
+})
+
+jest.mock('puppeteer', () => {
+  const screenshot = jest.fn()
+  return {
+    launch: () => ({
+      close: jest.fn(),
+      newPage: () => ({
+        setContent: jest.fn(),
+        $: () => ({
+          screenshot,
+        })
+      })
+    })
+  }
+})
