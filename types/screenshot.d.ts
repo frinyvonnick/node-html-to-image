2bee1f85c55a35758db07965c5fff0c37fc279ef
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-11-06 17:51:29 +0100

    :fire: Remove old types folder

--- types/screenshot.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import type { Page } from 'puppeteer';
-import type { NodeHtmlToImageOptions } from './options';
-
-declare function makeScreenshot(page: Page, { output, type, quality, encoding, content, html, beforeScreenshot, transparent, waitUntil, selector }: NodeHtmlToImageOptions): Promise<string | Buffer>;
-export default makeScreenshot;
