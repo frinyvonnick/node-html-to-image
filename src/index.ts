c6d4fa36b520166fd3b861ed47f1963009db33b3
Yvonnick Frin <frin.yvonnick@gmail.com>
2022-02-06 17:46:54 +0100

    :construction_worker: Add Typescript build to CI

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { nodeHtmlToImage } from "./main";
 /*
  * The following code is for interop between CommonJS and ESModule
  */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
 nodeHtmlToImage.default = nodeHtmlToImage;
+// @ts-ignore
 nodeHtmlToImage.__esModule = true;
+/* eslint-enable @typescript-eslint/ban-ts-comment */
 export = nodeHtmlToImage;
